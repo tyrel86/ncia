ead1d2658015e29e779a58b97ee0bfaf4c50b069
tyrel86 <tyrelrichey@gmail.com>
2012-10-16 20:58:03 -0600

    New dawn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 	end
 
 	def get_members_for_banner
-		@members_banner = Member.get_array_for_banner
+		@members_banner = Member.get_featured_array
 	end
 
 	def logged_in?
