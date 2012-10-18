f8eb58772a4116b8db167562b7c338dd94b7ab5f
root <root@li531-244.members.linode.com>
2012-10-18 15:15:24 +0000

    Changed to pure manifest

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,6 @@ Ncia::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-	config.assets.precompile += ['bootstrap-min.css','bootstrap-responsive-min.css']
 
   # Generate digests for assets URLs
   config.assets.digest = true
